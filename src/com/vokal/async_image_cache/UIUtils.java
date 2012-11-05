4544137a68f714d0aa78e748b5e61e78b843bb8c
Nick Fedesna <nick.fedesna@vokalinteractive.com>
2012-11-05 13:17:04 -0600

    Initial Android Library Project
    also includes LogUtils & condensed UIUtils

---
+++ src/com/vokal/async_image_cache/UIUtils.java
@@ -0,0 +1,189 @@
+/*
+ * Copyright 2012 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.vokal.async_image_cache;
+
+
+import android.annotation.TargetApi;
+import android.content.*;
+import android.content.res.Configuration;
+import android.graphics.Color;
+import android.os.Build;
+import android.support.v4.app.FragmentActivity;
+import android.text.Html;
+import android.text.TextUtils;
+import android.text.method.LinkMovementMethod;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Calendar;
+import java.util.TimeZone;
+
+/**
+ * An assortment of UI helpers.
+ */
+public class UIUtils {
+    /**
+     * Time zone to use when formatting times. To always use the
+     * phone local time, use {@link TimeZone#getDefault()}.
+     */
+    public static final TimeZone DEFAULT_TIME_ZONE = TimeZone.getTimeZone("America/Chicago");
+
+    private static final int SECOND_MILLIS = 1000;
+    private static final int MINUTE_MILLIS = 60 * SECOND_MILLIS;
+    private static final int HOUR_MILLIS = 60 * MINUTE_MILLIS;
+    private static final int DAY_MILLIS = 24 * HOUR_MILLIS;
+
+    public static boolean isSameDay(long time1, long time2) {
+        TimeZone.setDefault(DEFAULT_TIME_ZONE);
+
+        Calendar cal1 = Calendar.getInstance();
+        Calendar cal2 = Calendar.getInstance();
+        cal1.setTimeInMillis(time1);
+        cal2.setTimeInMillis(time2);
+        return cal1.get(Calendar.YEAR) == cal2.get(Calendar.YEAR) &&
+                cal1.get(Calendar.DAY_OF_YEAR) == cal2.get(Calendar.DAY_OF_YEAR);
+    }
+
+    public static String getTimeAgo(long time) {
+        if (time < 1000000000000L) {
+            // if timestamp given in seconds, convert to millis
+            time *= 1000;
+        }
+
+        long now = System.currentTimeMillis();
+        if (time > now || time <= 0) {
+            return null;
+        }
+
+        // TODO: localize
+        final long diff = now - time;
+        if (diff < MINUTE_MILLIS) {
+            return "just now";
+        } else if (diff < 2 * MINUTE_MILLIS) {
+            return "a minute ago";
+        } else if (diff < 50 * MINUTE_MILLIS) {
+            return diff / MINUTE_MILLIS + " minutes ago";
+        } else if (diff < 90 * MINUTE_MILLIS) {
+            return "an hour ago";
+        } else if (diff < 24 * HOUR_MILLIS) {
+            return diff / HOUR_MILLIS + " hours ago";
+        } else if (diff < 48 * HOUR_MILLIS) {
+            return "yesterday";
+        } else {
+            return diff / DAY_MILLIS + " days ago";
+        }
+    }
+
+    /**
+     * Populate the given {@link TextView} with the requested text, formatting
+     * through {@link Html#fromHtml(String)} when applicable. Also sets
+     * {@link TextView#setMovementMethod} so inline links are handled.
+     */
+    public static void setTextMaybeHtml(TextView view, String text) {
+        if (TextUtils.isEmpty(text)) {
+            view.setText("");
+            return;
+        }
+        if (text.contains("<") && text.contains(">")) {
+            view.setText(Html.fromHtml(text));
+            view.setMovementMethod(LinkMovementMethod.getInstance());
+        } else {
+            view.setText(text);
+        }
+    }
+
+    
+    public static ImageFetcher getImageFetcher(final FragmentActivity activity) {
+        // The ImageFetcher takes care of loading remote images into our ImageView
+        ImageFetcher fetcher = new ImageFetcher(activity);
+        fetcher.addImageCache(activity);
+        return fetcher;
+    }
+
+    
+    private static final int BRIGHTNESS_THRESHOLD = 130;
+
+    /**
+     * Calculate whether a color is light or dark, based on a commonly known
+     * brightness formula.
+     *
+     * @see {@literal http://en.wikipedia.org/wiki/HSV_color_space%23Lightness}
+     */
+    public static boolean isColorDark(int color) {
+        return ((30 * Color.red(color) +
+                59 * Color.green(color) +
+                11 * Color.blue(color)) / 100) <= BRIGHTNESS_THRESHOLD;
+    }
+
+    
+    public static void safeOpenLink(Context context, Intent linkIntent) {
+        try {
+            context.startActivity(linkIntent);
+        } catch (ActivityNotFoundException e) {
+            Toast.makeText(context, "Couldn't open link", Toast.LENGTH_SHORT) .show();
+        }
+    }
+
+    
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
+    public static void setActivatedCompat(View view, boolean activated) {
+        if (hasHoneycomb()) {
+            view.setActivated(activated);
+        }
+    }
+
+    public static boolean isGoogleTV(Context context) {
+    	return context.getPackageManager().hasSystemFeature("com.google.android.tv");
+    }
+
+    public static boolean hasFroyo() {
+        // Can use static final constants like FROYO, declared in later versions
+        // of the OS since they are inlined at compile time. This is guaranteed behavior.
+        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.FROYO;
+    }
+
+    public static boolean hasGingerbread() {
+        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.GINGERBREAD;
+    }
+
+    public static boolean hasHoneycomb() {
+        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB;
+    }
+
+    public static boolean hasHoneycombMR1() {
+        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR1;
+    }
+
+    public static boolean hasICS() {
+        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH;
+    }
+
+    public static boolean hasJellyBean() {
+        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN;
+    }
+
+    public static boolean isTablet(Context context) {
+        return (context.getResources().getConfiguration().screenLayout
+                & Configuration.SCREENLAYOUT_SIZE_MASK)
+                >= Configuration.SCREENLAYOUT_SIZE_LARGE;
+    }
+
+    public static boolean isHoneycombTablet(Context context) {
+        return hasHoneycomb() && isTablet(context);
+    }
+}
